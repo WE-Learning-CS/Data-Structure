c8b5a777940b6bbdd1b3c5ff00abdffaf53e0de7
qhsdnr0 <qhsdnr0@gmail.com>
2021-12-07 14:46:00 +0900

    add : Linear Search
    - 선형탐색 예시 코드
    
    - 보초법 적용 예시 코드

---
+++ chap03/Sentinel_method.py
@@ -0,0 +1,18 @@
+from typing import Any, Sequence
+from datetime import datetime
+
+def seq_search(a: Sequence, key: Any) -> int:
+    i = 0
+    a.append(key)
+    
+    while True:
+        
+        if a[i] == key:
+            return i if i != len(a) else -1
+        i += 1
+
+a = [i for i in range(1000000)]
+start = datetime.now()
+print(seq_search(a, 999999))
+end = datetime.now()
+print(end-start)
